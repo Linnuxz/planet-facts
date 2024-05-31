f0e07fca17751fc9fedcab838de06e358d34e31d
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-06-01 03:17:08 +0400

    fix PlanetList active styles bug

--- src/pages/PlanetList.tsx
+++ src/pages/PlanetList.tsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from 'react';
 import PlanetListElement from '../components/PlanetListElement';
 import { planets, TPlanet } from '../constants';
+import { useLocation } from 'react-router-dom';
 
 type TPlanetList = {
-    onPlanetClick: (planet: TPlanet) => void;
+    onPlanetClick?: (planet: TPlanet) => void;
 };
 
 const PlanetList = ({ onPlanetClick }: TPlanetList) => {
+    const handleClick = (planet: TPlanet) => {
+        if (onPlanetClick) {
+            onPlanetClick(planet);
+        }
+    };
+
+    const loc = useLocation();
+    const [selected, setSelected] = useState<string>(loc.pathname);
+
+    const handleSelect = (planet: string) => {
+        setSelected(planet);
+    };
+    useEffect(() => {
+        setSelected(loc.pathname.replace('/', ''));
+    }, [loc.pathname]);
+
     return (
-        <ul className="mt-[24px]">
+        <ul className="mt-[24px] md:mt-[10px] md:flex gap-[33px] justify-center">
             {planets.map((planet) => (
-                <PlanetListElement
+                <li
+                    className={`xl:${
+                        selected === planet.planet
+                            ? `border-t-[2px] border-solid ${planet.buttonColor}`
+                            : 'border-t-[2px] border-solid border-transparent'
+                    } relative`}
                     key={planet.id}
-                    planet={planet.planet}
-                    onPlanetClick={onPlanetClick}
-                />
+                >
+                    <div
+                        className={`${
+                            planet.buttonBgColor
+                        } w-full h-[4px] absolute top-[-10px] hidden ${
+                            selected === planet.planet ? 'xl:block' : 'hidden'
+                        }`}
+                    ></div>
+                    <PlanetListElement
+                        key={planet.id}
+                        planet={planet.planet}
+                        onPlanetClick={handleClick}
+                        onSelected={handleSelect}
+                    />
+                </li>
             ))}
         </ul>
     );
