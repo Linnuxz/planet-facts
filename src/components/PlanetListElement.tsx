4a2913246b6e822a0d0ccb51ec532eb53447bb98
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-06-01 03:23:11 +0400

    add onSelected to pass the selected page to the parent and add responsive styles

--- src/components/PlanetListElement.tsx
+++ src/components/PlanetListElement.tsx
@@ -6,9 +6,14 @@ import ArrowIcon from '../assets/icon-chevron.svg';
 type TPlanetElement = {
     planet: string;
     onPlanetClick: (planet: TPlanet) => void;
+    onSelected: (planet: string) => void;
 };
 
-const PlanetListElement = ({ planet, onPlanetClick }: TPlanetElement) => {
+const PlanetListElement = ({
+    planet,
+    onPlanetClick,
+    onSelected,
+}: TPlanetElement) => {
     const getPlanetObject = (): TPlanet | undefined => {
         return planets.find((item) => item.planet === planet);
     };
@@ -24,16 +29,21 @@ const PlanetListElement = ({ planet, onPlanetClick }: TPlanetElement) => {
                 planetObj.id !== planets.length
                     ? 'border-b border-white border-opacity-10'
                     : ''
-            }`}
-            onClick={() => onPlanetClick(planetObj)}
+            } md:border-none`}
+            onClick={() => {
+                onPlanetClick(planetObj);
+                onSelected(planetObj.planet);
+            }}
         >
             <div className="flex gap-[25px] items-center">
                 <div
-                    className={`${planetObj.color} w-[20px] h-[20px] rounded-full`}
+                    className={`${planetObj.color} w-[20px] h-[20px] rounded-full md:hidden`}
                 ></div>
-                <h2 className="text-white uppercase text-[17px] font-bold leading-[25px] tracking-[1.364px]">{planetObj.planet}</h2>
+                <h2 className="text-white uppercase text-[17px] md:text-[13px] font-bold leading-[25px] tracking-[1.364px]">
+                    {planetObj.planet}
+                </h2>
             </div>
-            <img src={ArrowIcon} alt="icon" />
+            <img src={ArrowIcon} alt="icon" className="md:hidden" />
         </NavLink>
     );
 };
